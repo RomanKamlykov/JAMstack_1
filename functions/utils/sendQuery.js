b68fd5db3d8b88e6c094179870556989b44f8f4b
RomanKamlykov <romankamlikov@gmail.com>
2020-10-10 22:50:33 +0300

    next commit

--- functions/utils/sendQuery.js
+++ functions/utils/sendQuery.js
@@ -1,12 +1,11 @@
 const axios = require('axios');
-const FAUNA_SECRET_KEY = "fnAD3Vtw8vACB33OqEVsxc3o7IEbvA3eL1HvBARx";
 const url = "https://graphql.fauna.com/graphql";
 
 module.exports = async (query, variables) => {
     const config = {
         url,
         method: 'POST',
-        headers: { Authorization: `Bearer ${FAUNA_SECRET_KEY}` },
+        headers: { Authorization: `Bearer ${process.env.FAUNA_SECRET_KEY}` },
         data: { query, variables }
     }
     
